c076248fc4993660e001caf00484f06bbc602886
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2021-03-08 22:38:50 +0000

    Begginings of a working ines cartridge implementation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod nes;
 mod ram;
 mod cartridge;
 mod ppu;
-
-use nes::BusDevice;
+mod mappers;
 
 use std::{thread, time};
 use std::cell::RefCell;
@@ -13,21 +12,9 @@ use std::rc::Rc;
 
 fn main() {
     let bus = Rc::new(RefCell::new(nes::Nes::new()));
-    let cartridge = Box::new(cartridge::Cartridge::new());
-    let mut ram = Box::new(ram::Ram::new());
+    let cartridge = Box::new(cartridge::Cartridge::new("Super_mario_brothers.nes"));
+    let ram = Box::new(ram::Ram::new());
     let ppu = Box::new(ppu::Ppu::new(Rc::clone(&bus)));
-
-    // Fill Ram
-    ram.write(0xfffd, 0x00);
-    ram.write(0x00fe, 0x01);
-
-    let program = [0xa5, 0xfe, 0x85, 0x00, 0xa5, 0xfe, 0x29, 0x03, 0x18, 0x69, 0x02, 0x85, 0x01, 0xa5, 0xfe, 0xa0, 0x00, 0x91, 0x00, 0x4c, 0x00, 0x06];
-    for (i, o) in program.iter().enumerate() {
-        ram.write((i as u16) + 0x0000, *o);
-    }
-
-    //println!("{:?}", cpu);
-    ram.print_range(0x0000..0x0100);
 
     {
         let mut mut_bus = bus.borrow_mut();
